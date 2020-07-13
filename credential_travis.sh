42204bfb14ca4c49006d3eb193b4e1236400e7f3
Tom Woodard <woodardt@amazon.com>
2020-07-13 14:00:36 -0700

    minor update

--- credential_travis.sh
+++ credential_travis.sh
@@ -8,3 +8,6 @@ cat > ~/.aws/credentials << EOL
 aws_access_key_id = ${AWS_ACCESS_KEY_ID}
 aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
 EOL
+
+sed -e s/ASK_ACCESS_TOKEN/${ASK_ACCESS_TOKEN}/g -e \
+    s/ASK_REFRESH_TOKEN/${ASK_REFRESH_TOKEN}/g ask_config.json > ~/.ask/cli_config
