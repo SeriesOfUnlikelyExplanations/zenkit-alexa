49f2cead590a0280ce43e4654bcd7dd89bf8a504
Tom Woodard <woodard.thomas@gmail.com>
2022-01-20 09:09:39 -0800

    minor update

--- lambda/householdList/client.js
+++ lambda/householdList/client.js
@@ -75,9 +75,10 @@ class SyncListClient {
    * @return {Promise}
    */
   async updateAlexaList(newUser = false) {
-    var [alexaLists, zenkitLists] = await Promise.all([
-      this.getAlexaLists(), this.zenKitClient.getListsInWorkspace()]);
-    if (!zenkitLists) {
+    try {
+      var [alexaLists, zenkitLists] = await Promise.all([
+        this.getAlexaLists(), this.zenKitClient.getListsInWorkspace()]);
+    } catch(e) {
       this.zenKitClient.setDefaultWorkspace(this.zenKitClient.workspaces[0].id);
       zenkitLists = await this.zenKitClient.getListsInWorkspace();
     }
